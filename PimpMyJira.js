99f47c22e8497294dbc2eab90d09e0731444b2f4
sonic1200 <sonic1200@gmail.com>
2019-02-11 14:29:53 -0500

    Update to version 3.2 -- JIRA 6.3.x only! New feature to extend OPSBAR

--- PimpMyJira.js
+++ PimpMyJira.js
@@ -2,11 +2,13 @@
 // @name        Pimp My Jira!
 // @description Enhances RapidBoard in Jira
 // @author       Nicolas Mivielle
-// @copyright    2016+, Nicolas Mivielle
+// @copyright    2018+, Nicolas Mivielle
 // @include     https://*/secure/RapidBoard.jspa*
+// @include     https://*/jira/browse*
 // @match        https://*/secure/RapidBoard.jspa*
+// @match        https://*/jira/browse*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version     3.0
+// @version     3.2
 // @grant       none
 // ==/UserScript==
 
@@ -24,6 +26,9 @@ var remove_type_icon=false;
 //This will move and 'stylize' all the extra fields that you configure in your board configuration. theses extra fields will be shown like Epics and Versions 'style'.
 var add_extra_fields=true;
 
+//This will update the jira card action toolbar to expand all workflows under the 'workflow' button with REAL buttons!
+var update_action_toolbar=true;
+
 //CSS statuses colors
 var blue = ".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-blue-gray jira-issue-status-lozenge-new aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium";
 var yellow = ".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium";
@@ -33,13 +38,21 @@ var flag = "{'background':'red','color':'white'}";
 //Dictionnary for all mapped statuses
 var statuses = { 'Reopened': blue,
                 'Open' : blue,
+                'Submitted' : blue,
+                'Need Approval' : blue,
                 'In Progress' : yellow,
+                'In Design' : yellow,
+                'In Development' : yellow,
+                'QC test Request' : yellow,
                 'Need More Info' : [yellow,flag],
+                'Blocked' : [yellow,flag],
                 'In Review' : yellow,
                 'Waiting For' : [yellow,flag],
                 'Suspended' : [yellow,flag],
                 'Work Completed' : yellow,
+                'In Dev Test' : yellow,
                 'Resolved' : green,
+                'Could Not Verified' : green,
                 'Closed' : green,
                 'Rejected' : green
                };
@@ -90,78 +103,112 @@ $j.browser = j_browser;
 
 $j(document).ready(function () {
   var timer=0;
-  
+  //updateToolbar();
   $j( "body" ).bind("DOMSubtreeModified", function(){
     if (timer)
       window.clearTimeout(timer);
-    
     if(! doing_modifications) {
         timer = window.setTimeout(function() {
             updateExtraFields();
-            //initvar();
-            //resizablePanel();
+            if (update_action_toolbar) {
+                updateToolbar();
+            }
         }, 100);
     }
     });
 });
 
+
 //FUNCTION TO RESIZE RIGHT PANEL
-function initvar(){
+window.addEventListener("load", installMissedSplitbar,false);
+
+    var currentStylesheet;
     var stateMouseDown = false;
     var mouseStartX = 0;
     var jiraStartWidth, jiraHeaderWidth;
-    var currentStylesheet;
     var dragBar = document.getElementById("ghx-detail-head");
     var detailView = document.getElementById("ghx-detail-view");
     var headerGroup = document.getElementById("ghx-column-header-group");
     var controlGroup = document.getElementById("ghx-detail-head");
+    var tmpElem = document.createElement('div');
+
+function installMissedSplitbar() {
+    dragBar = document.getElementById("ghx-detail-head");
+    detailView = document.getElementById("ghx-detail-view");
+    headerGroup = document.getElementById("ghx-column-header-group");
+    controlGroup = document.getElementById("ghx-detail-head");
+
+    tmpElem.innerHTML = '<span style="display: block;" id="js-sizer" class="ghx-sizer ui-resizable-handle ui-resizable-w" data-tooltip="Resize Detail View" original-title=""><span class="ghx-icon ghx-icon-sizer"></span></span>';
+
     var dragElem = tmpElem.childNodes[0];
+    if (controlGroup != null)  {
+        controlGroup.insertBefore(dragElem, controlGroup.childNodes[0]);
+        var currentWidth = localStorage.getItem('jiraWidth') || '400px';
+        currentStylesheet = document.createElement('style');
+        currentStylesheet.innerHTML = '#ghx-detail-view { width: ' + currentWidth + ' !important;}';
+        document.body.insertBefore(currentStylesheet, document.body.childNodes[0]);
+        dragBar.addEventListener("mousedown", startJiraDrag, false);
+        }
 }
 
-function startJiraDrag (ev) {
-        if (currentStylesheet){
+
+function startJiraDrag(ev) {
+        if (currentStylesheet) {
             document.body.removeChild(currentStylesheet);
         }
         stateMouseDown = true;
         mouseStartX = ev.pageX;
         jiraStartWidth = detailView.clientWidth;
         jiraHeaderWidth = headerGroup.clientWidth;
-        document.addEventListener ("mousemove" , continueJiraDrag , false);
-        document.addEventListener ("mouseup" , endJiraDrag , false);
-}
-function continueJiraDrag (ev){
+        document.addEventListener("mousemove", continueJiraDrag, false);
+        document.addEventListener("mouseup", endJiraDrag, false);
+    }
+
+function continueJiraDrag(ev) {
         var pX = ev.pageX;
         detailView.style.width = (jiraStartWidth + mouseStartX - pX) + "px";
         headerGroup.style.width = (jiraHeaderWidth - mouseStartX + pX) + "px";
-}
-function endJiraDrag () {
-        currentStylesheet = document.createElement('style');
-        currentStylesheet.innerHTML = '#ghx-detail-view{ width: ' + detailView.style.width + ' !important;}';
-        document.body.insertBefore(currentStylesheet, document.body.childNodes[0]);
-        localStorage.setItem('jiraWidth',detailView.style.width);
-        document.removeEventListener ("mousemove" , continueJiraDrag , false);
-        document.removeEventListener ("mouseup" , endJiraDrag , false);
-}
+    }
 
-function resizablePanel(){
-    var tmpElem = document.createElement('div');
-    tmpElem.innerHTML = '<span style="display: block;" id="js-sizer" class="ghx-sizer ui-resizable-handle ui-resizable-w" data-tooltip="Resize Detail View" original-title=""><span class="ghx-icon ghx-icon-sizer"></span></span>';
-    controlGroup.insertBefore(dragElem,controlGroup.childNodes[0]);
-    var currentWidth = localStorage.getItem('jiraWidth') || '400px';currentStylesheet = document.createElement('style');
-    currentStylesheet.innerHTML = '#ghx-detail-view{ width: ' + currentWidth + ' !important;}';
-    document.body.insertBefore(currentStylesheet, document.body.childNodes[0]);
-    dragBar.addEventListener ("mousedown" , startJiraDrag , false);
-}
+function endJiraDrag() {
+        currentStylesheet = document.createElement('style');
+        currentStylesheet.innerHTML = '#ghx-detail-view { width: ' + detailView.style.width + ' !important;}';
+        document.body.insertBefore(currentStylesheet, document.body.childNodes[0]);
+        localStorage.setItem('jiraWidth', detailView.style.width);
+        document.removeEventListener("mousemove", continueJiraDrag, false);
+        document.removeEventListener("mouseup", endJiraDrag, false);
+    }
+
+function updateToolbar() {
+
+    if ($j('ul#opsbar-opsbar-transitions').length) {
+
+        $j('.toolbar-item').each(function (index) {
+            if ($j(this).text().indexOf("Workflow") >= 0) {
+                $j(this).find('li.aui-list-item').each(function (index2) {
+                    //alert($j(this).text()); --DEBUG
+                    //alert($j(this).children().attr('href')); -- DEBUG
+                    //style='color:DarkBlue'
+                    $j('ul#opsbar-opsbar-transitions').append("<li class='toolbar-item'><a id=" + $j(this).children().attr('id') + "' class='toolbar-trigger issueaction-workflow-transition' href=" + $j(this).children().attr('href') + "><span class='trigger-label'>" + $j(this).text() + "</span></a></li>");
+                      });
+               $j(this).remove();
+          }
+
+         });
+
+      }
+
+    }
 
 function updateExtraFields(){
     try{
         doing_modifications=true;
-      
+
         $j('span.jira-issue-status-lozenge').each(function(index) {
             if ($j(this).text() == 'Suspended' || $j(this).text() == 'Waiting For' || $j(this).text() == 'Need More Info' )
                 $j(this).css({'background':'red','color':'white'});
         });
-      
+
         if ((add_extra_fields)) {
             
               $j('.ghx-plan-extra-fields').each(function (index) {
@@ -179,44 +226,44 @@ function updateExtraFields(){
                if (( ! $j(this).find('.ghx-extra-field-content').hasClass('aui-label ghx-label ghx-label-10')) && ( ! $j(this).find('.ghx-extra-field-content').hasClass('aui-lozenge') )) {
                     $j(this).find('.ghx-extra-field').each(function() {
 
+                        // Replace print glitch for field Last Public Comment.
+                        if ($j(this).text().indexOf('<!--') >= 0) {
+                                var newString = $j(this).html().substring($j(this).html().lastIndexOf( "--&gt;" ) +6, $j(this).html().length );
+                                $j(this).html('<span class="ghx-extra-field-content">' + newString + '</span>');
+                             }
+
                         switch ($j(this).text()) {
                             case 'None' :
                                 $j(this).remove();
                                 break;
                             case 'Reopened' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-blue-gray jira-issue-status-lozenge-new aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                break;
+                            case 'To Do' :
                             case 'Open' :
+                            case 'Submitted' :
+                            case 'Need Approval' :
                                 $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-blue-gray jira-issue-status-lozenge-new aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
                                 break;
-                            case 'In Progress' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                break;
-                            case 'Need More Info' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                $j(this).find('.ghx-extra-field-content').css({'background':'red','color':'white'});
-                                break;
+                            case 'In Progress':
+                            case 'In Design':
                             case 'In Review' :
+                            case 'Work Completed' :
+                            case 'In Development' :
+                            case 'In Dev Test' :
+                            case 'QC test Request' :
                                 $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
                                 break;
-                            case 'Waiting For' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                $j(this).find('.ghx-extra-field-content').css({'background':'red','color':'white'});
-                                break;
+                            case 'Need More Info':
+                            case 'Waiting For':
+                            case 'Blocked':
                             case 'Suspended' :
                                 $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
                                 $j(this).find('.ghx-extra-field-content').css({'background':'red','color':'white'});
                                 break;
-                            case 'Work Completed' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-yellow jira-issue-status-lozenge-indeterminate aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                break;
+                            case 'Closed':
+                            case 'Done':
                             case 'Resolved' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-green jira-issue-status-lozenge-done aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                break;
-                            case 'Closed' :
-                                $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-green jira-issue-status-lozenge-done aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
-                                break;
-                            case 'Rejected' :
+                            case 'Could Not Verified' :
+                            case 'Rejected':
                                 $j(this).find('.ghx-extra-field-content').toggleClass(".jira-issue-status-lozenge aui-lozenge jira-issue-status-lozenge-green jira-issue-status-lozenge-done aui-lozenge-subtle jira-issue-status-lozenge-max-width-medium");
                                 break;
                             default :
@@ -256,6 +303,7 @@ function updateExtraFields(){
                     // compute gradient and set to the current object
                     if (! $j(this).find('.ghx-flag').length) {
                         $j(this).find('.ghx-issue-content').css({ "background": "linear-gradient(to left, " + colorstart + " 75%, " + colorstop + ")"});
+                        //$j(this).prev().css({ "background": "linear-gradient(to left, " + colorstart + " 75%, " + colorstop + ")"});
                     }
                     if (remove_type_icon) {
                         $j(this).find('.ghx-type').remove();
